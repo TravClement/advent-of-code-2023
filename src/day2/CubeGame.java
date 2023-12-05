a4c7897f35ed58ec7dd97ab4c4fbd57560e66590
travis <travclement@gmail.com>
2023-12-04 20:10:40 -0800

    day 2 part 2

--- src/day2/CubeGame.java
+++ src/day2/CubeGame.java
@@ -7,7 +7,8 @@ import java.util.Scanner;
 
 public class CubeGame {
     public static void main(String[] args) throws FileNotFoundException {
-        int res = findGame("src/day2/input.txt", 12, 13, 14);
+//        int res = findGame("src/day2/input.txt", 12, 13, 14);
+        int res = findPower("src/day2/input.txt");
         System.out.println(res);
     }
 
@@ -24,6 +25,22 @@ public class CubeGame {
             if (game.maxRed <= wantedRed && game.maxBlue <= wantedBlue && game.maxGreen <= wantedGreen) {
                 total += game.gameNum;
             }
+        }
+
+        return total;
+    }
+
+    public static int findPower(String  inputPath) throws FileNotFoundException {
+        Scanner sc = new Scanner(new FileReader(inputPath));
+        int total = 0;
+        ArrayList<Game> games = new ArrayList<>();
+
+        while (sc.hasNextLine()) {
+            games.add(new Game(sc.nextLine()));
+        }
+
+        for (Game game : games) {
+            total += game.maxRed * game.maxGreen * game.maxBlue;
         }
 
         return total;
